9b07c6275ac8330876eb365dfdba5a632306af85
Jondal32 <manuelglade@web.de>
2021-05-03 15:28:04 +0200

    jetson inference add on

---
+++ core/jetson/distance_detection_jetson.py
@@ -0,0 +1,160 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+import numpy as np
+import cv2
+import time
+
+
+class DistanceDetection:
+
+    def __init__(self, camera_src):
+        self.camera_src = camera_src
+        self.camera = None
+        self.fps = 0
+        self.amount_detected = 0
+        self.MIN_DISTANCE = 50
+        self.net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+        self.MIN_CONF = 0.3
+        self.NMS_THRESH = 0.3
+
+        # info for cards
+        self.fps = 0
+        self.violations = 0
+        self.amount_detected = 0
+
+    def close(self):
+        if self.camera is not None:
+            self.camera.release()
+            self.camera = None
+
+    def open(self):
+        self.camera = cv2.VideoCapture(self.camera_src)
+
+    def status(self):
+        return self.camera is not None
+
+    def get_fps(self):
+        return self.fps
+
+    def generateFrames(self):
+        while True:
+            (grabbed, frame) = self.camera.read()
+            start = time.time()
+
+            # if the frame was not grabbed, then we have reached the end
+            # of the stream
+            if not grabbed:
+                self.camera = cv2.VideoCapture(self.camera_src)
+
+                continue
+
+            results = self.detect_people(frame)
+
+            violate = set()
+
+            # ensure there are *at least* two people detections (required in
+            # order to compute our pairwise distance maps)
+            if len(results) >= 2:
+                # extract all centroids from the results and compute the
+                # Euclidean distances between all pairs of the centroids
+                centroids = np.array([r[2] for r in results])
+                D = dist.cdist(centroids, centroids, metric="euclidean")
+
+                # loop over the upper triangular of the distance matrix
+                for i in range(0, D.shape[0]):
+                    for j in range(i + 1, D.shape[1]):
+                        # check to see if the distance between any two
+                        # centroid pairs is less than the configured number
+                        # of pixels
+                        if D[i, j] < self.MIN_DISTANCE:
+                            # update our violation set with the indexes of
+                            # the centroid pairs
+                            violate.add(i)
+                            violate.add(j)
+
+            # loop over the results
+            for (i, (prob, bbox, centroid)) in enumerate(results):
+                # extract the bounding box and centroid coordinates, then
+                # initialize the color of the annotation
+                (startX, startY, endX, endY) = bbox
+                (cX, cY) = centroid
+                color = (0, 255, 0)
+
+                # if the index pair exists within the violation set, then
+                # update the color
+                if i in violate:
+                    color = (0, 0, 255)
+
+                # draw (1) a bounding box around the person and (2) the
+                # centroid coordinates of the person,
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                cv2.circle(frame, (cX, cY), 5, color, 1)
+
+            self.violations = len(violate)
+            self.amount_detected = len(results)
+
+            # draw the total number of social distancing violations on the
+            # output frame
+
+            # check to see if the output frame should be displayed to our
+            # screen
+            self.fps = 1.0 / (time.time() - start)
+            (flag, encodedImage) = cv2.imencode(".jpg", frame)
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+
+    def detect_people(self, frame):
+        # grab the dimensions of the frame and  initialize the list of
+        # results
+        height = frame.shape[0]
+        width = frame.shape[1]
+        results = []
+
+        cuda_frame = jetson.utils.cudaFromNumpy(frame)
+        detections = self.net.Detect(cuda_frame, width, height, overlay='none')
+
+        # initialize our lists of detected bounding boxes, centroids, and
+        # confidences, respectively
+        boxes = []
+        centroids = []
+        confidences = []
+
+        for detection in detections:
+            confidence = float(detection.Confidence)
+
+            # if the confidence is above a threshold
+            if confidence > self.MIN_CONF:
+                classID = detection.ClassID
+
+                # proceed only if the object detected is indeed a human
+                if classID == 1:
+                    # get coordinates of the bbox
+                    left = detection.Left
+                    top = detection.Top
+                    right = detection.Right
+                    bottom = detection.Buttom
+                    width = detection.Width
+                    height = detection.Height
+
+                    boxes.append([left, top, width, height])
+                    centroids.append((detection.Center[0], detection.Center[1]))
+                    confidences.append(confidence)
+
+        # NMS on bounding boxes
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.MIN_CONF, self.NMS_THRESH)
+
+        # ensure at least one detection exists
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                # extract the bounding box coordinates
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+
+                # update our results list to consist of the person
+                # prediction probability, bounding box coordinates,
+                # and the centroid
+                r = (confidences[i], (x, y, x + w, y + h), centroids[i])
+                results.append(r)
+
+        # return the list of results
+        return results
---
+++ core/jetson/mask_detection_jetson.py
@@ -0,0 +1,198 @@
+import threading
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+import cv2
+import imutils
+import time
+from datetime import datetime
+import jetson.inference
+import jetson.utils
+
+lock = threading.Lock()
+from app import mysql
+
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            # add the face and bounding boxes to their respective
+            # lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return locs, preds
+
+
+class Stream:
+
+    def __init__(self, camera_src):
+        self.camera_src = 0  # camera_src
+        self.camera = None
+        self.prev_messages = ""
+        self.fps = 0
+        self.amount_detected = 0
+        self.withMask = 0
+        self.withoutMask = 0
+
+        # jeweils 1 damit das pie chart funktioniert und wir nicht durch 0 teilen
+        self.maskFrames = 0
+        self.noMaskFrames = 0
+
+        self.stream_on = False
+        self.fpsFilt = 0
+        dispW = 1280
+        dispH = 720
+        flip = 2
+        self.camera = cv2.VideoCapture(
+            'nvarguscamerasrc wbmode=3 tnr-mode=2 tnr-strength=1 ee-mode=2 ee-strength=1 !  video/x-raw(memory:NVMM), '
+            'width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=' + str(
+                flip) + ' ! video/x-raw, width=' + str(dispW) + ', height=' + str(
+                dispH) + ', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! videobalance contrast=1.5 '
+                         'brightness=-.2 saturation=1.2 !  appsink')
+        self.net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+
+        self.timeStamp = time.time()
+        self.font = cv2.FONT_HERSHEY_COMPLEX
+
+    def close(self):
+        self.stream_on = False
+        savePieChartData(self.maskFrames, self.noMaskFrames)
+
+    def open(self):
+        self.stream_on = True
+
+    def status(self):
+        return self.stream_on
+
+    def get_fps(self):
+        return self.fps
+
+    def generateFrames(self, faceNet, maskNet):
+
+        while True:
+            if self.stream_on:
+                ret, img = self.camera.read()
+                if not ret:
+                    print("break")
+                    break
+                height = img.shape[0]
+                width = img.shape[1]
+                # img = camera.Capture()
+
+                # frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA).astype(np.float32)
+                frame = jetson.utils.cudaFromNumpy(img)
+
+                detections = self.net.Detect(frame, width, height, overlay='none')
+                for detect in detections:
+                    # print(detect)
+                    ID = detect.ClassID
+                    top = detect.Top
+                    left = detect.Left
+                    bottom = detect.Bottom
+                    right = detect.Right
+                    item = self.net.GetClassDesc(ID)
+                    # print(item, top, left, bottom, right)
+
+                    img = cv2.rectangle(img,
+                                        (int(left), int(top)),
+                                        (int(right), int(bottom)), color=(0, 0, 255), thickness=2)
+                # display.RenderOnce(img,width,height)
+                dt = time.time() - self.timeStamp
+                timeStamp = time.time()
+                fps = 1 / dt
+                self.fpsFilt = .9 * self.fpsFilt + .1 * fps
+                # print(str(round(fps,1))+' fps')
+                self.fps = round(self.fpsFilt, 1)
+                cv2.putText(img, str(self.fps) + ' fps', (0, 30), self.font, 1, (0, 0, 255), 2)
+
+                (flag, encodedImage) = cv2.imencode(".jpg", img)
+                yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+
+
+def savePieChartData(maskFrames, noMaskFrames):
+    try:
+
+        cur = mysql.connection.cursor()
+        result = cur.execute(
+            "SELECT * FROM maskproportion WHERE date = CURDATE()")
+
+        # wenn wir ein Ergebnis auf der Abfrage bekommen benutzen wir es ansonsten pushen wir ein neues ergebnis in die datenbank
+        if result > 0:
+            re = cur.fetchall()
+            # die alten Frames zu den neuen hinzufügen
+            updatedMaskFrames = maskFrames + re[0]["maskFrames"]
+            updatedNoMaskFrames = noMaskFrames + re[0]["noMaskFrames"]
+
+            cur.execute("UPDATE maskproportion SET maskFrames=%s, noMaskFrames=%s WHERE date=%s",
+                        (updatedMaskFrames, updatedNoMaskFrames, datetime.now().strftime('%Y-%m-%d')))
+
+            mysql.connection.commit()
+
+            # Close connection
+            cur.close()
+
+        else:
+            cur.execute("INSERT INTO maskproportion(date,maskFrames,noMaskFrames) VALUES(%s, %s, %s)",
+                        (datetime.now().strftime('%Y-%m-%d'), maskFrames, noMaskFrames))
+
+            # Commit to DB
+            mysql.connection.commit()
+
+            # Close connection
+            cur.close()
+
+
+    except ValueError:
+        print("Value Error beim speichern vom pie chart!")
